d974f884bd5491b8396fd4585a5e591e94b016d1
Yosef <yosef.leibman@mail.utoronto.ca>
2019-05-22 13:30:02 -0400

    Not using ws definition, might include in extras. Using string literal def instead of string def

--- grammar.js
+++ grammar.js
@@ -25,170 +25,192 @@ module.exports = grammar({
   name: 'wdl',
 
   rules: {
-    // ws: $ => /((%x20) | (%x9) | (%xD) | (%xA))+/,
-    ws: $ => /(' ')+/,
-    identifier: $ => /[a-zA-Z][a-zA-Z0-9_]+/ ,
+    identifier: $ => prec(-1, /[a-zA-Z][a-zA-Z0-9_]+/),
     integer: $ => /[1-9][0-9]*|0[xX][0-9a-fA-F]+|0[0-7]*/,
     float: $ => /(([0-9]+)?\.([0-9]+)|[0-9]+\.|[0-9]+)([eE][-+]?[0-9]+)?/,
-    boolean: $ => choice('true', 'false'),
+    boolean: $ => prec(2, token(choice('true', 'false'))),
     object_type: $ => 'Object',
-    string: $ =>
-    choice(/'([^\\\'\n]|\\[\\"\'nrbtfav\?]|\\[0-7]{1,3}|\\x[0-9a-fA-F]+|\\[uU]([0-9a-fA-F]{4})([0-9a-fA-F]{4})?)*'/,
-    /"([^\\\"\n]|\\[\\"\'nrbtfav\?]|\\[0-7]{1,3}|\\x[0-9a-fA-F]+|\\[uU]([0-9a-fA-F]{4})([0-9a-fA-F]{4})?)*"/),
-
-
-    _type: $ => seq(choice($.primitive_type,
-      $.array_type,
-      $.map_type,
-      $.object_type),
-      optional($.type_postfix_quantifier)),
-    primitive_type: $ => choice(
+    primitive_type: $ => prec.left(3, choice(
         'Boolean',
         'Int',
         'Float',
         'File',
         'String'
-            ),
+      )),
+    // string: $ => prec(2,
+    // choice(/'([\\\'\n]|\\[\\"\'nrbtfav\?]|\\[0-7]{1,3}|\\x[0-9a-fA-F]+|\\[uU]([0-9a-fA-F]{4})([0-9a-fA-F]{4})?)*'/,
+    // /"([\\\"\n]|\\[\\"\'nrbtfav\?]|\\[0-7]{1,3}|\\x[0-9a-fA-F]+|\\[uU]([0-9a-fA-F]{4})([0-9a-fA-F]{4})?)*"/))
 
-    array_type: $ => seq('Array',
-        '[',
-        choice($.primitive_type, $.object_type, $.array_type),
-         ']'
-       ),
-    map_type: $ => seq('Map', '[', $.primitive_type, ',',
-    choice($.primitive_type, $.array_type, $.map_type, $.object_type), ']'),
+    string_literal: $ => seq(
+      '"',
+      repeat(choice(
+        token.immediate(prec(1, /[^\\"\n]+/)),
+        $.escape_sequence
+      )),
+      '"'
+    ),
 
-    type_postfix_quantifier: $ => choice('?', '+'),
+    escape_sequence: $ => token.immediate(seq(
+      '\\',
+      choice(
+        /[^xuU]/,
+        /\d{2,3}/,
+        /x[0-9a-fA-F]{2,}/,
+        /u[0-9a-fA-F]{4}/,
+        /U[0-9a-fA-F]{8}/
+      )
+    ))
 
-     fully_qualified_name: $ => seq($.identifier,
-       repeat(seq('.', $.identifier))),
-     namespaced_identifier: $ => seq($.identifier,
-       repeat(seq('.', $.identifier))),
-      number: $ => /\d+/,
 
-      declaration: $ => seq($._type, $.identifier,
-                optional(
-                  seq('=', $.expression) ) ),
+
+
       /// center_operator: $ => choice('%', '/', '+', '*', '-', '<', '=<', '>', '>=',
       // '==', '!=', '&&', '||'),
-      expression: $ => prec(3, choice(
-                              seq($.expression, '.', $.expression),
-                              seq($.expression, '[', $.expression, ']'),
-                              seq($.expression,'(',
-                              optional(
-                                seq($.expression,
-                                  repeat(
-                                    seq(',',$.expression)
-                                  ),')'
-                                )
-                              )),
-                              seq('if', $.expression, 'then',
-                              $.expression, 'else',
-                              $.expression),
-                              // seq($.expression, $.center_operator, $.expression),,
-                                choice($.string,
-                                  $.integer,
-                                  $.float,
-                                  $.boolean,
-                                  $.identifier))),
-      parentheses_expression: $ => seq('(', $.expression, ')'),
-      dictionary_expression: $ => seq('{',
-        repeat(
-          seq($.expression, ':', $.expression)
-        ),
-      '}'),
-      bracket_expression: $ =>  seq('[', repeat($.expression), ']'),
-      math_expression: $ => choice(
-        prec.left(PREC.ADD, seq($.expression, '+', $.expression)),
-        prec.left(PREC.ADD, seq($.expression, '-', $.expression)),
-        prec.left(PREC.MULTIPLY, seq($.expression, '*', $.expression)),
-        prec.left(PREC.MULTIPLY, seq($.expression, '/', $.expression)),
-        prec.left(PREC.MULTIPLY, seq($.expression, '%', $.expression)),
-        prec.right(PREC.UNARY, seq('-', $.expression)),
-        prec.right(PREC.UNARY, seq('+', $.expression))),
-        logical_expression: $ => choice(
-        prec.left(PREC.LOGICAL_OR, seq($.expression, '||', $.expression)),
-        prec.left(PREC.LOGICAL_AND, seq($.expression, '&&', $.expression)),
-        prec.left(PREC.UNARY, seq('!', $.expression))
-      ),
-      equality_expression: $ => prec.left(PREC.EQUAL, seq(
-      $.expression, choice('==', '!='), $.expression
-      )),
+      // expression: $ => prec(3, choice(
+      //                         seq($.expression, '.', $.expression),
+      //                         seq($.expression, '[', $.expression, ']'),
+      //                         seq($.expression,'(',
+      //                         optional(
+      //                           seq($.expression,
+      //                             repeat(
+      //                               seq(',',$.expression)
+      //                             ),')'
+      //                           )
+      //                         )),
+      //                         seq('if', $.expression, 'then',
+      //                         $.expression, 'else',
+      //                         $.expression),
+      //                         // seq($.expression, $.center_operator, $.expression),,
+      //                           choice($.string,
+      //                             $.integer,
+      //                             $.float,
+      //                             $.boolean,
+      //                             $.identifier))),
+      // parentheses_expression: $ => seq('(', $.expression, ')'),
+      // dictionary_expression: $ => seq('{',
+      //   repeat(
+      //     seq($.expression, ':', $.expression)
+      //   ),
+      // '}'),
+      // bracket_expression: $ =>  seq('[', repeat($.expression), ']'),
+      // math_expression: $ => choice(
+      //   prec.left(PREC.ADD, seq($.expression, '+', $.expression)),
+      //   prec.left(PREC.ADD, seq($.expression, '-', $.expression)),
+      //   prec.left(PREC.MULTIPLY, seq($.expression, '*', $.expression)),
+      //   prec.left(PREC.MULTIPLY, seq($.expression, '/', $.expression)),
+      //   prec.left(PREC.MULTIPLY, seq($.expression, '%', $.expression)),
+      //   prec.right(PREC.UNARY, seq('-', $.expression)),
+      //   prec.right(PREC.UNARY, seq('+', $.expression))),
+      //   logical_expression: $ => choice(
+      //   prec.left(PREC.LOGICAL_OR, seq($.expression, '||', $.expression)),
+      //   prec.left(PREC.LOGICAL_AND, seq($.expression, '&&', $.expression)),
+      //   prec.left(PREC.UNARY, seq('!', $.expression))
+      // ),
+      // equality_expression: $ => prec.left(PREC.EQUAL, seq(
+      // $.expression, choice('==', '!='), $.expression
+      // )),
+      //
+      // relational_expression: $ => prec.left(PREC.RELATIONAL, seq(
+      // $.expression, choice('<', '>', '<=', '>='), $.expression
+      // ))
 
-      relational_expression: $ => prec.left(PREC.RELATIONAL, seq(
-      $.expression, choice('<', '>', '<=', '>='), $.expression
-      )),
 
-      document: $ => prec(2, repeat1(choice($.import, $.task, $.workflow))),
-      import: $ => seq('import', repeat1($.ws),
-                      $.string,
-                      optional(
-                      seq(
-                        repeat1($.ws),
-                        'as',
-                        repeat1($.ws), $.identifier)
-                      )
-                    ),
-      task: $ => prec(1, seq('task', repeat1($.ws),
-            $.identifier,
-            repeat($.ws),
-            '{', repeat($.ws), repeat($.declaration),
-            $.task_sections, repeat($.ws), '}')),
-
-      task_sections: $ => prec(1, repeat1(choice($.command, $.runtime, $.task_output, $.parameter_meta, $.meta))),
-
-      command: $ =>choice(
-                  seq('command', repeat($.ws), '{', /(0xA | 0xD)*/, repeat1($.command_part), repeat1($.ws), '}'),
-                  seq('command', repeat($.ws), '<<<', /(0xA | 0xD)*/, repeat1($.command_part), repeat1($.ws), '>>>')),
-
-      command_part: $ => choice($.command_part_string, $.command_part_var),
-      command_part_string: $ => /\{+/,
-      command_part_var: $ => seq(/\{/, repeat($.var_option), $.expression, '}'),
-
-      var_option: $ => seq($.var_option_key, repeat($.ws), '=', repeat($.ws), $.var_option_value),
-      var_option_key: $ => choice('sep', 'true', 'false', 'quote', 'default'),
-      var_option_value: $ => $.expression,
-
-      task_output: $ => seq('output', repeat($.ws), '{', repeat(seq(repeat($.ws), $.task_output_kv, repeat($.ws))), '}'),
-      task_output_kv: $ => seq($._type, $.identifier, repeat($.ws), '=', repeat($.ws), $.string),
-
-      runtime: $ => seq('runtime', repeat($.ws), '{', repeat(seq(repeat($.ws), $.runtime_kv, repeat($.ws))), '}'),
-      runtime_kv: $ => seq($.identifier, repeat($.ws), '=', repeat($.ws), $.expression),
-
-      parameter_meta: $ => seq('parameter_meta', repeat($.ws),
-      '{', repeat(seq(repeat($.ws), $.parameter_meta_kv, repeat($.ws))), '}'),
-      parameter_meta_kv: $ => seq($.identifier, repeat($.ws), '=', repeat($.ws), $.string),
-
-      meta: $ => seq('meta', repeat($.ws), '{', repeat(seq(repeat($.ws), $.meta_kv, repeat($.ws))), '}'),
-      meta_kv: $ => seq($.identifier, repeat($.ws), '=', repeat($.ws), $.string),
-
-      workflow: $ => seq('workflow', repeat1($.ws), $.identifier, repeat($.ws),  '{', repeat($.ws), repeat($.workflow_element), repeat($.ws), '}'),
-      workflow_element: $ => choice($.call, $.loop, $.conditional, $.declaration, $.scatter, $.parameter_meta, $.meta),
-
-      call: $ => seq('call', repeat($.ws), $.namespaced_identifier, repeat1($.ws),
-                            optional(seq('as', $.identifier)),
-                            repeat($.ws), optional($.call_body)),
-
-      call_body: $ => seq('{', repeat($.ws), optional($.inputs), repeat($.ws), '}'),
-      inputs: $ => seq('input', repeat($.ws), ':', repeat($.ws), $.variable_mappings),
-
-      variable_mappings: $ => seq($.variable_mapping_kv, repeat(seq(',', $.variable_mapping_kv))),
-      variable_mapping_kv: $ => seq($.identifier, repeat($.ws), '=', repeat($.ws), $.expression),
-
-      scatter: $ => seq('scatter',
-                      repeat($.ws), '(', repeat($.ws),
-                       repeat($.scatter_iteration_statment), repeat($.ws),  ')', repeat($.ws), $.scatter_body),
-      scatter_iteration_statment: $ => seq($.identifier, repeat($.ws), 'in', repeat($.ws), $.expression),
-      scatter_body: $ => seq('{', repeat($.ws),
-                            repeat($.workflow_element),
-                            repeat($.ws), '}'),
-      loop: $ => seq('while', '(', $.expression, ')', '{', repeat($.workflow_element), '}'),
-
-      conditional: $ => seq('if', '(', $.expression, ')', '{', repeat($.workflow_element), '}'),
-
-      wf_parameter_meta: $ => seq('parameter_meta', repeat($.ws), '{', repeat(seq(repeat($.ws), $.wf_parameter_meta_kv, repeat($.ws))), '}'),
-      wf_parameter_meta_kv: $ => seq($.identifier, repeat($.ws), '=', repeat($.ws), $.string),
-      wf_meta: $ => seq('meta', repeat($.ws), '{', repeat(seq(repeat($.ws), $.wf_meta_kv, repeat($.ws))), '}'),
-      wf_meta_kv: $ => seq($.identifier, repeat($.ws), '=', repeat($.ws), $.string)
+      // ,
+      // document: $ => prec(2, repeat1(choice($.import, $.task, $.workflow))),
+      // import: $ => seq('import', repeat1($.ws),
+      //                 $.string,
+      //                 optional(
+      //                 seq(
+      //                   repeat1($.ws),
+      //                   'as',
+      //                   repeat1($.ws), $.identifier)
+      //                 )
+      //               ),
+      // task: $ => prec(1, seq('task', repeat1($.ws),
+      //       $.identifier,
+      //       repeat($.ws),
+      //       '{', repeat($.ws), repeat($.declaration),
+      //       $.task_sections, repeat($.ws), '}')),
+      //
+      // task_sections: $ => prec(1, repeat1(choice($.command, $.runtime, $.task_output, $.parameter_meta, $.meta))),
+      //
+      // command: $ =>choice(
+      //             seq('command', repeat($.ws), '{', /(0xA | 0xD)*/, repeat1($.command_part), repeat1($.ws), '}'),
+      //             seq('command', repeat($.ws), '<<<', /(0xA | 0xD)*/, repeat1($.command_part), repeat1($.ws), '>>>')),
+      //
+      // command_part: $ => choice($.command_part_string, $.command_part_var),
+      // command_part_string: $ => /\{+/,
+      // command_part_var: $ => seq(/\{/, repeat($.var_option), $.expression, '}'),
+      //
+      // var_option: $ => seq($.var_option_key, repeat($.ws), '=', repeat($.ws), $.var_option_value),
+      // var_option_key: $ => choice('sep', 'true', 'false', 'quote', 'default'),
+      // var_option_value: $ => $.expression,
+      //
+      // task_output: $ => seq('output', repeat($.ws), '{', repeat(seq(repeat($.ws), $.task_output_kv, repeat($.ws))), '}'),
+      // task_output_kv: $ => seq($._type, $.identifier, repeat($.ws), '=', repeat($.ws), $.string),
+      //
+      // runtime: $ => seq('runtime', repeat($.ws), '{', repeat(seq(repeat($.ws), $.runtime_kv, repeat($.ws))), '}'),
+      // runtime_kv: $ => seq($.identifier, repeat($.ws), '=', repeat($.ws), $.expression),
+      //
+      // parameter_meta: $ => seq('parameter_meta', repeat($.ws),
+      // '{', repeat(seq(repeat($.ws), $.parameter_meta_kv, repeat($.ws))), '}'),
+      // parameter_meta_kv: $ => seq($.identifier, repeat($.ws), '=', repeat($.ws), $.string),
+      //
+      // meta: $ => seq('meta', repeat($.ws), '{', repeat(seq(repeat($.ws), $.meta_kv, repeat($.ws))), '}'),
+      // meta_kv: $ => seq($.identifier, repeat($.ws), '=', repeat($.ws), $.string),
+      //
+      // workflow: $ => seq('workflow', repeat1($.ws), $.identifier, repeat($.ws),  '{', repeat($.ws), repeat($.workflow_element), repeat($.ws), '}'),
+      // workflow_element: $ => choice($.call, $.loop, $.conditional, $.declaration, $.scatter, $.parameter_meta, $.meta),
+      //
+      // call: $ => seq('call', repeat($.ws), $.namespaced_identifier, repeat1($.ws),
+      //                       optional(seq('as', $.identifier)),
+      //                       repeat($.ws), optional($.call_body)),
+      //
+      // call_body: $ => seq('{', repeat($.ws), optional($.inputs), repeat($.ws), '}'),
+      // inputs: $ => seq('input', repeat($.ws), ':', repeat($.ws), $.variable_mappings),
+      //
+      // variable_mappings: $ => seq($.variable_mapping_kv, repeat(seq(',', $.variable_mapping_kv))),
+      // variable_mapping_kv: $ => seq($.identifier, repeat($.ws), '=', repeat($.ws), $.expression),
+      //
+      // scatter: $ => seq('scatter',
+      //                 repeat($.ws), '(', repeat($.ws),
+      //                  repeat($.scatter_iteration_statment), repeat($.ws),  ')', repeat($.ws), $.scatter_body),
+      // scatter_iteration_statment: $ => seq($.identifier, repeat($.ws), 'in', repeat($.ws), $.expression),
+      // scatter_body: $ => seq('{', repeat($.ws),
+      //                       repeat($.workflow_element),
+      //                       repeat($.ws), '}'),
+      // loop: $ => seq('while', '(', $.expression, ')', '{', repeat($.workflow_element), '}'),
+      //
+      // conditional: $ => seq('if', '(', $.expression, ')', '{', repeat($.workflow_element), '}'),
+      //
+      // wf_parameter_meta: $ => seq('parameter_meta', repeat($.ws), '{', repeat(seq(repeat($.ws), $.wf_parameter_meta_kv, repeat($.ws))), '}'),
+      // wf_parameter_meta_kv: $ => seq($.identifier, repeat($.ws), '=', repeat($.ws), $.string),
+      // wf_meta: $ => seq('meta', repeat($.ws), '{', repeat(seq(repeat($.ws), $.wf_meta_kv, repeat($.ws))), '}'),
+      // wf_meta_kv: $ => seq($.identifier, repeat($.ws), '=', repeat($.ws), $.string),
+      //
+      // array_type: $ => seq('Array',
+      //     '[',
+      //     choice($.primitive_type, $.object_type, $.array_type),
+      //      ']'
+      //    ),
+      // map_type: $ => seq('Map', '[', $.primitive_type, ',',
+      // choice($.primitive_type, $.array_type, $.map_type, $.object_type), ']'),
+      //
+      // type_postfix_quantifier: $ => choice('?', '+'),
+      //
+      //  fully_qualified_name: $ => seq($.identifier,
+      //    repeat(seq('.', $.identifier))),
+      //  namespaced_identifier: $ => seq($.identifier,
+      //    repeat(seq('.', $.identifier))),
+      //   number: $ => /\d+/,
+      //
+      //   declaration: $ => seq($._type, $.identifier,
+      //             optional(
+      //               seq('=', $.expression) ) ),
+      // _type: $ => seq(choice($.primitive_type,
+      //   $.array_type,
+      //   $.map_type,
+      //   $.object_type),
+      //   optional($.type_postfix_quantifier)),
       }
 });
