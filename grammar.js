e57e373d8a42a33812ac48e044fc2deb73448668
yoyosef <yosefleibman@gmail.com>
2019-09-18 19:12:01 -0400

    Changed precedence of identifier

--- grammar.js
+++ grammar.js
@@ -50,7 +50,7 @@ module.exports = grammar({
 
   rules: {
     definition: $ => choice($.expression, $.type, $.runtime, $.document, $.call),
-    identifier: $ => prec(-1, /[a-zA-Z][a-zA-Z0-9_]+/),
+    identifier: $ => /[a-zA-Z][a-zA-Z0-9_]+/,
     integer: $ => /[1-9][0-9]*|0[xX][0-9a-fA-F]+|0[0-7]*/,
     float: $ => /(([0-9]+)?\.([0-9]+)|[0-9]+\.|[0-9]+)([eE][-+]?[0-9]+)?/,
     boolean: $ => choice('true', 'false'),
